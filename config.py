568f538bd746db52b51fe01e059af7b75e32efec
hub5 <1965047976@qq.com>
2023-12-10 17:14:12 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+class Config:
+    input_length=6
+    output_length=6
+
+    rh_max=100.0
+    rh_min=0.0
+
+    tp_max=318.0
+    tp_min=249.0
+
+    ws_max=33.0
+    ws_min=0.0
+
+    train_num=5840
+    test_num=2912
+
+    height=80
+    wight=80
