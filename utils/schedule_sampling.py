9dfc73a8cc2678e6985e5112c1ac91f928944979
hub5 <1965047976@qq.com>
2023-12-10 17:22:00 +0800

    Add files via upload

---
+++ utils/schedule_sampling.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def schedule_sampling(args,eta, itr):
+    zeros = np.zeros((args.batch_size,
+                      args.total_length - args.input_length - 1,
+                      args.size,
+                      args.size,
+                      1))
+    if not args.scheduled_sampling:
+        return 0.0, zeros
+
+    if itr < args.sampling_stop_iter:
+        eta -= args.sampling_changing_rate
+    else:
+        eta = 0.0
+    random_flip = np.random.random_sample(
+        (args.batch_size, args.total_length - args.input_length - 1))
+    true_token = (random_flip < eta)
+    ones = np.ones((args.size,args.size,1))
+    zeros = np.zeros((args.size,args.size,1))
+    real_input_flag = []
+    for i in range(args.batch_size):
+        for j in range(args.total_length - args.input_length - 1):
+            if true_token[i, j]:
+                real_input_flag.append(ones)
+            else:
+                real_input_flag.append(zeros)
+    real_input_flag = np.array(real_input_flag)
+    real_input_flag = np.reshape(real_input_flag,
+                           (args.batch_size,
+                            args.total_length - args.input_length - 1,
+                            args.size,args.size,1))
+    return eta, real_input_flag
